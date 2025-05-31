e4cd8dc7b0cdf2b14aae7ef528b6846a80159196
Navneet Ranjan <ridestatus07@gmail.com>
2025-05-31 23:17:50 +0530

    Mobile Preview

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js
 document.addEventListener("DOMContentLoaded", () => {
     const keys = document.querySelectorAll(".key");
     
@@ -10,17 +9,24 @@ document.addEventListener("DOMContentLoaded", () => {
         playSound(event.keyCode);
     });
 
-    const element1 = document.querySelector('.key');
-    function disableHoverEffects(){
-        element1.classList.add('disable-hover')
-    }
-
-    // const element = document.getElementById('key');
-
+    // const element1 = document.querySelector('.key');
+    // function disableHoverEffects(){
+    //     element1.classList.add('disable-hover')
+    // }
+    // const element2 = document.querySelector('.key');
+    // function enableHoverEffects(){
+    //      element2.classList.add('enable-hover')
+    // }
+// enableHoverEffects();
+// disableHoverEffects();
+    
     function playSound(keyCode) {
         let audio;
 
+   
+
         if((keyCode==65) || event.key === 'a' || event.key==='A' ){
+            audio = new Audio("ads/key01.mp3")
             const element = document.getElementById('key1');
             element.style.transform = 'scale(1.05)'
             element.style.backgroundColor = '#81cfd6'
@@ -28,10 +34,11 @@ document.addEventListener("DOMContentLoaded", () => {
                 element.style.removeProperty("transform")
                 element.style.removeProperty("background-color")
             },200)
-            audio = new Audio("ads/key01.mp3")
-            disableHoverEffects();
+                        
+            
 
         }else if((keyCode==83) || event.key === 's' || event.key === 'S'){
+            audio = new Audio("ads/key02.mp3")
             const element = document.getElementById('key2');
             element.style.transform = 'scale(1.05)'
             element.style.backgroundColor = '#81cfd6'
@@ -39,9 +46,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 element.style.removeProperty("transform")
                 element.style.removeProperty("background-color")
             },200)
-            audio = new Audio("ads/key02.mp3")
+            
 
         }else if((keyCode==68) || event.key === 'd' || event.key === 'D'){
+            audio = new Audio("ads/key03.mp3")
             const element = document.getElementById('key3');
             element.style.transform = 'scale(1.05)'
             element.style.backgroundColor = '#81cfd6'
@@ -49,9 +57,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 element.style.removeProperty("transform")
                 element.style.removeProperty("background-color")
             },200)
-            audio = new Audio("ads/key03.mp3")
+            
 
         }else if((keyCode==70 ) || event.key === 'f' || event.key === 'F'){
+             audio = new Audio("ads/key04.mp3")
             const element = document.getElementById('key4');
             element.style.transform = 'scale(1.05)'
             element.style.backgroundColor = '#81cfd6'
@@ -59,9 +68,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 element.style.removeProperty("transform")
                 element.style.removeProperty("background-color")
             },200)
-            audio = new Audio("ads/key04.mp3")
+           
 
         }else if((keyCode==71) || event.key === 'g' || event.key === 'G'){
+            audio = new Audio("ads/key05.mp3")
             const element = document.getElementById('key5');
             element.style.transform = 'scale(1.05)'
             element.style.backgroundColor = '#81cfd6'
@@ -69,9 +79,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 element.style.removeProperty("transform")
                 element.style.removeProperty("background-color")
             },200)
-            audio = new Audio("ads/key05.mp3")
+            
 
         }else if((keyCode==72) || event.key === 'h' || event.key === 'H'){
+            audio = new Audio("ads/key06.mp3")
             const element = document.getElementById('key6');
             element.style.transform = 'scale(1.05)'
             element.style.backgroundColor = '#81cfd6'
@@ -79,9 +90,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 element.style.removeProperty("transform")
                 element.style.removeProperty("background-color")
             },200)
-            audio = new Audio("ads/key06.mp3")
+            
 
         }else if((keyCode==74) || event.key === 'j' || event.key === 'J'){
+            audio = new Audio("ads/key07.mp3")
             const element = document.getElementById('key7');
             element.style.transform = 'scale(1.05)'
             element.style.backgroundColor = '#81cfd6'
@@ -89,9 +101,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 element.style.removeProperty("transform")
                 element.style.removeProperty("background-color")
             },200)
-            audio = new Audio("ads/key07.mp3")
+            
 
         }else if((keyCode==75) || event.key === 'k' || event.key === 'K'){
+             audio = new Audio("ads/key08.mp3")
             const element = document.getElementById('key8');
             element.style.transform = 'scale(1.05)'
             element.style.backgroundColor = '#81cfd6'
@@ -99,10 +112,11 @@ document.addEventListener("DOMContentLoaded", () => {
                 element.style.removeProperty("transform")
                 element.style.removeProperty("background-color")
             },200)
-            audio = new Audio("ads/key08.mp3")
+           
 
         }
-
+        
         audio.play();
+        
     }
 });
